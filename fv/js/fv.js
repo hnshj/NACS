52f993b0cf6bb2dc66ef5eb7e485f8d766a9b40b
hnshj <66721294+hnshj@users.noreply.github.com>
2022-04-23 11:42:11 +0900

    ファイル名変更に伴う修正

--- fv/js/fv.js
+++ fv/js/fv.js
@@ -19,7 +19,7 @@ function back(){
     title.innerHTML="お疲れ様でした";
     contents.innerHTML="NACSを使用する準備がでできました。\r\n次へをクリックするとNACSが開始します。";
 } else {
-    window.location.href = "./public/kadai.html";
+    window.location.href = "./public/NACS.html";
 }
 
 }
@@ -38,15 +38,18 @@ function next(){
     var x = localStorage.getItem('x')
 }else if (x == 0){
     title.innerHTML="NACSをご利用前に以下の内容に同意して下さい";
-    icon.innerHTML= '<img src="#" width="300" height="200">';
+    icon.innerHTML= '<img src="public/img/kiyaku.png" width="300" height="200">';
     progress.innerHTML="1/3"
     contents.innerHTML="";
-    kiyaku.innerHTML="<b>プライバシーポリシー</b><br>NACSは以下の内容を取得することがあります。<br>・OSの情報<br>・ブラウザの情報<br>この情報は、ご利用者様のデバイス内で処理され、ご利用者様の同意なくNACSに送信されることはありません。<br><b>免責事項</b><br>当プログラムを利用することによってご利用者様に損害が発生した場合、NACSは、その損害の代償にかかわらず、いかなる保証も行いません。<br>このサービスは、事前に通知なく、停止する場合があります。<br>NACSのあらゆるプログラムのあらゆる権利は、NACSに帰属します。我々の許可無くプログラムを複製、公開、配布することを禁じます。<br>同意しない場合は、この画面を閉じてください。同意されない場合、NACSをご利用いただけません。";
+    kiyaku.innerHTML="<b>プライバシーポリシー(2022年4月22日作成）</b><br>NACSは以下の内容を取得することがあります。"
+    +"<br>・OSの情報<br>・ブラウザの情報<br>この情報は、個人を特定できない形で、NACSに送信される場合があります。"
+    +"NACSは、いただいた個人情報を、ご利用者様の許可なく、外部に提供することはありません。"
+    +"また、情報はNACS内でのみ共有し、部内全体で利用することはありません。"
     backButton.innerHTML='<button value="戻る" onclick="back()">戻る</button>';
     nextButton.innerHTML="同意";
  }else if (x == 1){
     title.innerHTML="推奨環境";
-    icon.innerHTML= '<img src="#" width="300" height="200">';
+    icon.innerHTML= '<img src="public/img/osusume.png" width="300" height="200">';
     contents.innerHTML="以下の環境で実行されることをお勧めします。この条件を満たしていなくても、"
     +"NACSの実行は、可能ですが、正常にNACSが動作しない可能性があります。あらかじめご了承ください。"
     +"<br>学校のiPad(ロイロノート、OptimalWeb)"
@@ -60,7 +63,7 @@ function next(){
     nextButton.innerHTML="次へ";
  } else if (x == 11){
     title.innerHTML="お疲れ様でした";
-    icon.innerHTML= '<img src="#" width="300" height="200">';
+    icon.innerHTML= '<img src="public/img/finish.png" width="300" height="200">';
     progress.innerHTML="3/3"
     contents.innerHTML="NACSを使用する準備がでできました。\r\nスタートをクリックするとNACSが開始します。";
     kiyaku.innerHTML="";
@@ -70,7 +73,7 @@ function next(){
     var storage = localStorage;
     storage.setItem('visit',visit);
 } else {
-    window.location.href = "./public/kadai.html";
+    window.location.href = "./public/NACS.html";
     localStorage.removeItem("x");
 }
 
