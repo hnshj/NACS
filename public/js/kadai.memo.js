8792a6fed814b821711246e1778ec937e40c2c0d
hnshj <66721294+hnshj@users.noreply.github.com>
2022-05-09 23:54:25 +0900

    不具合の修正と機能更新プログラム

--- public/js/kadai.memo.js
+++ public/js/kadai.memo.js
@@ -2,6 +2,9 @@
  *kadai.memo.js
  *メモ表示モジュール
  */
+  //---読み込み---
+  //load();
+
 kadai.memodx = (function (){
     'user strict';
 
@@ -12,19 +15,11 @@ kadai.memodx = (function (){
         main_html : String()
         + '<button class="kadai-memo-calendar">課題</button>'
         + '<button class="kadai-memo-schedule">時間割</button>'
-        + '<canvas'
-        + 'id="draw-area"'
-        + 'top="100px"'
-        + 'left="10px"'
-        + 'width="1000px"'
-        + 'height="500px"'
-        + 'style="border: 1px solid #000000;">'
-        + '</canvas>'
-        + '<div>'
-        + '<button id="clear-button" class="clear-button">全消し</button>'
-        + '<button id="eraser-button" class="eraser-button">消しゴム</button>'
+        + '<div class="memo">'
+        + '<form name="memomodule">'
+        + '<textarea name="memo" class="kadai-memo-memo"></textarea>'
+        + '</form>'
         + '</div>'
-        + '<script src="./memo.js"></script>'
     },
     stateMap = {
       $container : null,
@@ -53,6 +48,7 @@ kadai.memodx = (function (){
     $.gevent.publish('changeCalendar', [obj]);
 
     window.alert("calendar");
+    save();
   }
 
   onSchedule = function () {
@@ -64,6 +60,7 @@ kadai.memodx = (function (){
     $.gevent.publish('schedule', [obj]);
 
     window.alert("schedule");
+    save();
   }
 
     //---パブリックメソッド---
@@ -79,121 +76,13 @@ kadai.memodx = (function (){
     $container.html( configMap.main_html );
     stateMap.$container = $container;
     setJqueryMap();
+    jqueryMap.$calendar
+     .click( onCalendar );
+    jqueryMap.$schedule
+     .click( onSchedule );
+    return true;
   }
-
-    //メモ用プログラム
-    // ページの読み込みが完了したらコールバック関数が呼ばれる
-// ※コールバック: 第2引数の無名関数(=関数名が省略された関数)
-window.addEventListener('load', () => {
-    const canvas = document.querySelector('#draw-area');
-    // contextを使ってcanvasに絵を書いていく
-    const context = canvas.getContext('2d');
-  
-    // 直前のマウスのcanvas上のx座標とy座標を記録する
-    const lastPosition = { x: null, y: null };
-  
-    // マウスがドラッグされているか(クリックされたままか)判断するためのフラグ
-    let isDrag = false;
-  
-    // 絵を書く
-    function draw(x, y) {
-      // マウスがドラッグされていなかったら処理を中断する。
-      // ドラッグしながらしか絵を書くことが出来ない。
-      if(!isDrag) {
-        return;
-      }
-  
-      // 「context.beginPath()」と「context.closePath()」を都度draw関数内で実行するよりも、
-      // 線の描き始め(dragStart関数)と線の描き終わり(dragEnd)で1回ずつ読んだほうがより綺麗に線画書ける
-  
-      // 線の状態を定義する
-      // MDN CanvasRenderingContext2D: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineJoin
-      context.lineCap = 'round'; // 丸みを帯びた線にする
-      context.lineJoin = 'round'; // 丸みを帯びた線にする
-      context.lineWidth = 5; // 線の太さ
-      context.strokeStyle = 'black'; // 線の色
-  
-      // 書き始めは lastPosition.x, lastPosition.y の値はnullとなっているため、
-      // クリックしたところを開始点としている。
-      // この関数(draw関数内)の最後の2行で lastPosition.xとlastPosition.yに
-      // 現在のx, y座標を記録することで、次にマウスを動かした時に、
-      // 前回の位置から現在のマウスの位置まで線を引くようになる。
-      if (lastPosition.x === null || lastPosition.y === null) {
-        // ドラッグ開始時の線の開始位置
-        context.moveTo(x, y);
-      } else {
-        // ドラッグ中の線の開始位置
-        context.moveTo(lastPosition.x, lastPosition.y);
-      }
-      // context.moveToで設定した位置から、context.lineToで設定した位置までの線を引く。
-      // - 開始時はmoveToとlineToの値が同じであるためただの点となる。
-      // - ドラッグ中はlastPosition変数で前回のマウス位置を記録しているため、
-      //   前回の位置から現在の位置までの線(点のつながり)となる
-      context.lineTo(x, y);
-  
-      // context.moveTo, context.lineToの値を元に実際に線を引く
-      context.stroke();
-  
-      // 現在のマウス位置を記録して、次回線を書くときの開始点に使う
-      lastPosition.x = x;
-      lastPosition.y = y;
-    }
-  
-    // canvas上に書いた絵を全部消す
-    function clear() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-  
-    // マウスのドラッグを開始したらisDragのフラグをtrueにしてdraw関数内で
-    // お絵かき処理が途中で止まらないようにする
-    function dragStart(event) {
-      // これから新しい線を書き始めることを宣言する
-      // 一連の線を書く処理が終了したらdragEnd関数内のclosePathで終了を宣言する
-      context.beginPath();
-  
-      isDrag = true;
-    }
-    // マウスのドラッグが終了したら、もしくはマウスがcanvas外に移動したら
-    // isDragのフラグをfalseにしてdraw関数内でお絵かき処理が中断されるようにする
-    function dragEnd(event) {
-      // 線を書く処理の終了を宣言する
-      context.closePath();
-      isDrag = false;
-  
-      // 描画中に記録していた値をリセットする
-      lastPosition.x = null;
-      lastPosition.y = null;
-    }
-  
-    // マウス操作やボタンクリック時のイベント処理を定義する
-    function initEventHandler() {
-      const clearButton = document.querySelector('#clear-button');
-      clearButton.addEventListener('click', clear);
-  
-      canvas.addEventListener('mousedown', dragStart);
-      canvas.addEventListener('mouseup', dragEnd);
-      canvas.addEventListener('mouseout', dragEnd);
-      canvas.addEventListener('mousemove', (event) => {
-        // eventの中の値を見たい場合は以下のようにconsole.log(event)で、
-        // デベロッパーツールのコンソールに出力させると良い
-        // console.log(event);
-  
-        draw(event.layerX, event.layerY);
-      });
-    }
-  
-    // イベント処理を初期化する
-    initEventHandler();
-
-
-  jqueryMap.$calendar
-    .click( onCalendar );
-  jqueryMap.$memo
-    .click( onMemo );
-  return true;
-  });
     
-
 
 
 return {
--- public/js/kadai.storage.js
+++ public/js/kadai.storage.js
@@ -2,3 +2,21 @@ function usersave(){
 localStorage.setItem('id', id);
 localStorage.setItem('pass', pass);
 }
+
+//---ここから先メモ---
+//保存
+function save() {
+    var MemoData = document.memomodule.memo.value;
+    localStorage.setItem('MemoData', MemoData);
+}
+
+// 読込
+function load() {
+    var MemoData = "";
+    if(!localStorage.getItem('MemoData')) {
+        MemoData = "メモは登録されていません。";
+    } else {
+        MemoData = localStorage.getItem('MemoData');
+    }
+    document.memomodule.memo.value = MemoData;
+}
